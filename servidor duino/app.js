a3b79eb6269bed22815f988d3fbde8d2283a9787
Diego Trujillo <72467997+dtrujillog-umg@users.noreply.github.com>
2023-08-18 18:29:32 -0600

    18082023
    Control de los servos

--- servidor duino/app.js
+++ servidor duino/app.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const SerialPort = require("serialport");
+const Readline = require('@serialport/parser-readline'); // Importa el parser
 
 app.use(express.static(__dirname + "/public"));
 
@@ -10,6 +11,9 @@ const arduinoPort = "COM4"; // Cambia esto al puerto correcto
 const port = new SerialPort(arduinoPort, {
   baudRate: 9600,
 });
+
+// Define un parser para leer los datos del puerto serial línea por línea
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
 
 io.on("connection", (socket) => {
   console.log("Un cliente se ha conectado");
@@ -49,35 +53,25 @@ io.on("connection", (socket) => {
     const command = data === "ON" ? "ON6\n" : "OFF6\n";
     enviarComandoSerial(command);
   });
-
-  // Agregar control para los servos
+//control de los servos
   socket.on("servo1", (data) => {
-    console.log("Comando servo 1:", data);
-    const command = data === "CLOSE1\n";
+    console.log("Comando Servo 1:", data);
+    const command = data === "OPEN1" ? "OPEN1\n" : "CLOSE1\n";
     enviarComandoSerial(command);
   });
 
-  socket.on("CLOSE1", () => {
-    console.log("Cerrando Servo 1");
-    const command = "CLOSE1\n";
+  socket.on("servo2", (data) => {
+    console.log("Comando Servo 2:", data);
+    const command = data === "OPEN2" ? "OPEN2\n" : "CLOSE2\n";
     enviarComandoSerial(command);
   });
+});
 
-  socket.on("OPEN2", () => {
-    console.log("Abriendo Servo 2");
-    const command = "OPEN2\n";
-    enviarComandoSerial(command);
-  });
-
-  socket.on("CLOSE2", () => {
-    console.log("Cerrando Servo 2");
-    const command = "CLOSE2\n";
-    enviarComandoSerial(command);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Un cliente se ha desconectado");
-  });
+// Maneja los datos del puerto serial cuando llegan
+parser.on('data', (data) => {
+  data = data.trim(); // Elimina espacios en blanco alrededor de los datos
+  console.log("Datos recibidos desde Arduino:", data);
+  // Agrega aquí tu lógica para manejar los datos recibidos desde Arduino
 });
 
 function enviarComandoSerial(command) {
